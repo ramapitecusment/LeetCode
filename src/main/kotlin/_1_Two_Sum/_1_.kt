ffe8588e023772415891c1acd5a8f3a0812a482c
iolchu <ramil.iolchu@kaspi.kz>
2022-09-21 09:38:46 +0600

    Solve the first task

--- src/main/kotlin/_1_Two_Sum/_1_.kt
+++
@@ -1,2 +0,0 @@
-package _1_Two_Sum
-
---
+++ src/main/kotlin/_Top_100_Liked_Questions/_1_Two_Sum/Solution.kt
@@ -0,0 +1,24 @@
+package _Top_100_Liked_Questions._1_Two_Sum
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val difMap = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, i ->
+            val dif = target - i
+            if (difMap[i] != null) return intArrayOf(difMap[i]!!, index)
+            else difMap[dif] = index
+        }
+        return intArrayOf()
+    }
+}
+
+fun main() {
+    val solution1 = Solution().twoSum(intArrayOf(2, 7, 11, 15), 9)
+    println(solution1[0].toString() + " " + solution1[1].toString())
+
+    val solution2 = Solution().twoSum(intArrayOf(3, 2, 4), 6)
+    println(solution2[0].toString() + " " + solution2[1].toString())
+
+    val solution3 = Solution().twoSum(intArrayOf(3, 3), 6)
+    println(solution3[0].toString() + " " + solution3[1].toString())
+}
