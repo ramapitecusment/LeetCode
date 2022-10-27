0f98677d66bf2ba4d7a4a82699bf6dc504fa1c1e
iolchu <ramil.iolchu@kaspi.kz>
2022-10-27 14:44:22 +0600

    Solution 25

--- src/main/kotlin/_Top_100_Liked_Questions/_26_Remove_Duplicates_from_Sorted_Array/Solution.kt
+++ src/main/kotlin/_Top_100_Liked_Questions/_26_Remove_Duplicates_from_Sorted_Array/Solution.kt
@@ -1,21 +1,52 @@
 package _Top_100_Liked_Questions._26_Remove_Duplicates_from_Sorted_Array
 
-class Solution {
-    fun removeDuplicates(nums: IntArray): Int {
-        var uniqueValues = 0
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-        for (i in nums.indices - 1) {
-            if (nums[i] == nums[i + 1]) uniqueValues++
+class Solution {
+
+//    fun removeDuplicates(nums: IntArray): Int {
+//        var insertIndex = 1
+//
+//        for (i in 1 until nums.size) {
+//            if (nums[i] != nums[i - 1]) {
+//                nums[insertIndex++] = nums[i]
+//            }
+//        }
+//
+//        return insertIndex
+//    }
+
+    fun removeDuplicates(nums: IntArray): Int {
+        var insertIndex = 1
+
+        nums.forEach {
+            if (it > nums[insertIndex - 1]) nums[insertIndex++] = it
         }
 
-        return uniqueValues
+        return insertIndex
     }
 }
 
 fun main() {
-    val solution1 = Solution().removeDuplicates(intArrayOf(1, 1, 2))
-    val solution2 = Solution().removeDuplicates(intArrayOf(0, 0, 1, 1, 2, 2, 3, 3, 4))
+    val nums1 = intArrayOf(1, 1, 1)
+    val solution1 = Solution().removeDuplicates(nums1) // 1
+    val nums2 = intArrayOf(1, 1, 2)
+    val solution2 = Solution().removeDuplicates(nums2) // 2
+    val nums3 = intArrayOf(0, 0, 1, 1, 2, 2, 3, 3, 4, 4)
+    val solution3 = Solution().removeDuplicates(nums3) // 5
+    val nums4 = intArrayOf(0, 1, 2, 2, 3, 4)
+    val solution4 = Solution().removeDuplicates(nums4) // 5
 
     println(solution1)
+    nums1.forEach(::print)
+    println()
     println(solution2)
+    nums2.forEach(::print)
+    println()
+    println(solution3)
+    nums3.forEach(::print)
+    println()
+    println(solution4)
+    nums4.forEach(::print)
+    println()
 }
