80fa322aff58d15833eb68819e694e40acf1b2cd
iolchu <ramil.iolchu@gmail.com>
2023-01-16 18:08:11 +0600

    task 69

---
+++ src/main/kotlin/_Top_100_Liked_Questions/_69_Sqrtx/Solution.kt
@@ -0,0 +1,15 @@
+package _Top_100_Liked_Questions._69_Sqrtx
+
+class Solution {
+    fun mySqrt(x: Int): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val solution1 = Solution().mySqrt(4) // 2
+    val solution2 = Solution().mySqrt(8) // 2
+
+    println(solution1)
+    println(solution2)
+}
