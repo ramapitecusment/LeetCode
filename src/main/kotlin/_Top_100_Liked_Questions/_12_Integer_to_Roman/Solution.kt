c8abd07a880835c2ed9a7df41e67c7486448c24d
iolchu <ramil.iolchu@gmail.com>
2023-02-27 17:45:47 +0600

    added an efficient way to solve

--- src/main/kotlin/_Top_100_Liked_Questions/_12_Integer_to_Roman/Solution.kt
+++ src/main/kotlin/_Top_100_Liked_Questions/_12_Integer_to_Roman/Solution.kt
@@ -1,9 +1,12 @@
 package _Top_100_Liked_Questions._12_Integer_to_Roman
 
+import java.lang.StringBuilder
+
 // https://leetcode.com/problems/integer-to-roman/
 
 //class Solution {
-//
+
+
 //    fun intToRoman(num: Int): String {
 //
 //        var result = ""
@@ -35,18 +38,46 @@ package _Top_100_Liked_Questions._12_Integer_to_Roman
 //
 //        return result
 //    }
+
+//    private val thousands = arrayOf("", "M", "MM", "MMM")
+//    private val hundreds = arrayOf("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
+//    private val tens = arrayOf("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
+//    private val ones = arrayOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+//
+//    fun intToRoman(num: Int): String =
+//        thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10]
+
 //}
 
 class Solution {
 
-    private val thousands = arrayOf("", "M", "MM", "MMM")
-    private val hundreds = arrayOf("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
-    private val tens = arrayOf("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
-    private val ones = arrayOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+    private val symbols = mapOf(
+        "M" to 1000,
+        "CM" to 900,
+        "D" to 500,
+        "CD" to 400,
+        "C" to 100,
+        "XC" to 90,
+        "L" to 50,
+        "XL" to 40,
+        "X" to 10,
+        "IX" to 9,
+        "V" to 5,
+        "IV" to 4,
+        "I" to 1,
+    )
 
-    fun intToRoman(num: Int): String =
-        thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10]
-
+    fun intToRoman(num: Int): String {
+        val builder = StringBuilder()
+        var number = num
+        symbols.forEach { (symbol, value) ->
+            while (number >= value) {
+                number -= value
+                builder.append(symbol)
+            }
+        }
+        return builder.toString()
+    }
 }
 
 fun main() {
