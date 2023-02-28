2c84037702890d0f5ddba2ba7d84954c92ba4967
iolchu <ramil.iolchu@gmail.com>
2023-02-28 09:08:57 +0600

    added a new solution

--- src/main/kotlin/_Top_100_Liked_Questions/_13_Roman_to_Integer/Solution.kt
+++ src/main/kotlin/_Top_100_Liked_Questions/_13_Roman_to_Integer/Solution.kt
@@ -2,43 +2,6 @@ package _Top_100_Liked_Questions._13_Roman_to_Integer
 
 // https://leetcode.com/problems/roman-to-integer/
 
-class Solution {
-
-    val romanMap = hashMapOf(
-        'I' to 1,
-        'V' to 5,
-        'X' to 10,
-        'L' to 50,
-        'C' to 100,
-        'D' to 500,
-        'M' to 1000
-    )
-
-//    fun romanToInt(s: String): Int {
-//        var sum = 0
-//        var previousValue = 1000
-//        s.forEach { c ->
-//            var value: Int = romanMap[c] ?: 0
-//            if (value > previousValue) value -= previousValue * 2
-//            sum += value
-//            previousValue = value
-//        }
-//        return sum
-//    }
-
-    fun romanToInt(s: String): Int {
-        var total = 0
-        var lastValue = romanMap[s[s.length - 1]] ?: 0
-        for (i in s.length - 1 downTo 0) {
-            val value = romanMap[s[i]] ?: 0
-            if (value >= lastValue) total += value
-            else total -= value
-            lastValue = value
-        }
-        return total
-    }
-}
-
 //class Solution {
 //
 //    var values: MutableMap<String, Int> = HashMap()
@@ -68,6 +31,60 @@ class Solution {
 //        return total
 //    }
 //}
+
+//class Solution {
+//
+//    val romanMap = hashMapOf(
+//        'I' to 1,
+//        'V' to 5,
+//        'X' to 10,
+//        'L' to 50,
+//        'C' to 100,
+//        'D' to 500,
+//        'M' to 1000
+//    )
+//
+//    fun romanToInt(s: String): Int {
+//        var value: Int
+//        var lastValue = 0
+//        var result = 0
+//        s.forEach {
+//            value = romanMap[it]!!
+//            result += if (value <= lastValue) value
+//            else value - 2 * lastValue
+//            lastValue = value
+//        }
+//        return result
+//    }
+//
+//}
+
+class Solution {
+
+    private val romanMap = hashMapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+
+    fun romanToInt(s: String): Int {
+        var value: Int
+        var total = 0
+        var lastValue = romanMap[s[s.length - 1]] ?: 0
+        for (i in s.length - 1 downTo 0) {
+            value = romanMap[s[i]] ?: 0
+            if (value >= lastValue) total += value
+            else total -= value
+            lastValue = value
+        }
+        return total
+    }
+
+}
 
 fun main() {
     val solution1 = Solution().romanToInt("III") // 3
