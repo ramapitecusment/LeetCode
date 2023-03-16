35721830e8d99b9ef46ff79683301c6652f9c9c4
iolchu <ramil.iolchu@gmail.com>
2023-03-16 18:39:51 +0600

    Update Task7.kt

--- src/main/kotlin/_Yandex/_1_linear_search/Task7.kt
+++ src/main/kotlin/_Yandex/_1_linear_search/Task7.kt
@@ -1,5 +1,7 @@
 package _Yandex._1_linear_search
 
+import kotlin.math.max
+
 /*
     Игра PirCraft происходит в двумерном мире, который состоит
     из блоков размером 1 на 1 метр. Остров игрока представляет
@@ -13,10 +15,30 @@ package _Yandex._1_linear_search
 */
 
 fun solution(island: Array<Int>): Int {
-    return 0
+    var result = 0
+    var maxPos = 0
+    island.forEachIndexed { index, value ->
+        if (value > maxPos) maxPos = index
+    }
+    var curMax = 0
+    for (i in 0 until maxPos) {
+        if (island[i] > curMax) curMax = island[i]
+        else result += curMax - island[i]
+    }
+    curMax = 0
+    for (i in island.size - 1 downTo maxPos) {
+        if (island[i] > curMax) curMax = island[i]
+        else result += curMax - island[i]
+    }
+    return result
 }
 
 fun main() {
-    val solution1 = intArrayOf(3, 1, 4, 3, 5, 1, 1, 3, 1) // 7
-    val solution2 = intArrayOf(3, 2, 5, 2, 2, 5, 2, 2, 3) // 9
+    val solution1 = solution(arrayOf(3, 1, 4, 3, 5, 1, 1, 3, 1)) // 7
+    val solution2 = solution(arrayOf(3, 2, 5, 2, 2, 5, 2, 2, 3)) // 9
+    val solution3 = solution(arrayOf(3, 2, 5, 2, 2, 5, 2, 2, 3, 3, 2, 3)) // 10
+
+    println(solution1)
+    println(solution2)
+    println(solution3)
 }
