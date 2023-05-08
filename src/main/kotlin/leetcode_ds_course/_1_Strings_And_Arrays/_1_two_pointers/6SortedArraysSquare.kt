4043b097f9a4bf3b98767e20dd28fcc76ac5318f
Ramapitecusment <ramil.iolchu@gmail.com>
2023-05-08 14:29:00 +0600

    Create 6SortedArraysSquare.kt

---
+++ src/main/kotlin/leetcode_ds_course/_1_Strings_And_Arrays/_1_two_pointers/6SortedArraysSquare.kt
@@ -0,0 +1,69 @@
+package leetcode_ds_course._1_Strings_And_Arrays._1_two_pointers
+
+import Assert.Assert.assertEquals
+import java.util.*
+import kotlin.math.abs
+
+//class SortedArraysSquare {
+//
+//    fun sortedSquares(nums: IntArray): IntArray {
+//        var i = 0
+//        var j = 0
+//        val stack = Stack<Int>()
+//        while (i < nums.size) {
+//            val curr = nums[i]
+//            val square = curr * curr
+//            if (nums[i] < 0) {
+//                stack.add(curr * curr)
+//            } else {
+//                while (stack.isNotEmpty()) {
+//                    if (stack.last() < square) nums[j++] = stack.pop()
+//                    else break
+//                }
+//                nums[j++] = square
+//            }
+//            i++
+//        }
+//
+//        while (stack.isNotEmpty()) {
+//            nums[j++] = stack.pop()
+//        }
+//        return nums
+//    }
+//
+//}
+
+class SortedArraysSquare {
+
+    fun sortedSquares(nums: IntArray): IntArray {
+        val size = nums.size
+        val array = IntArray(size)
+        var left = 0
+        var right = size - 1
+        for (i in size - 1 downTo 0) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                array[i] = nums[left] * nums[left]
+                left++
+            } else {
+                array[i] = nums[right] * nums[right]
+                right--
+            }
+        }
+        return array
+    }
+
+}
+
+fun main() {
+    val array1 = intArrayOf(-4, -1, 0, 3, 10)
+    val solution1 = SortedArraysSquare().sortedSquares(array1)
+    assertEquals(listOf(0, 1, 9, 16, 100), solution1.toList())
+
+    val array2 = intArrayOf(-7, -3, 2, 3, 11)
+    val solution2 = SortedArraysSquare().sortedSquares(array2)
+    assertEquals(listOf(4, 9, 9, 49, 121), solution2.toList())
+
+    val array3 = intArrayOf(-1)
+    val solution3 = SortedArraysSquare().sortedSquares(array3)
+    assertEquals(listOf(1), solution3.toList())
+}
