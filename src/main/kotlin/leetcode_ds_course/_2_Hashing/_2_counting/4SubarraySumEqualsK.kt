a666d94ed7e986cfb6fab79515ad430f11d3d87c
iolchu <ramil.iolchu@gmail.com>
2023-05-18 09:15:08 +0600

    Update 4SubarraySumEqualsK.kt

--- src/main/kotlin/leetcode_ds_course/_2_Hashing/_2_counting/4SubarraySumEqualsK.kt
+++ src/main/kotlin/leetcode_ds_course/_2_Hashing/_2_counting/4SubarraySumEqualsK.kt
@@ -5,6 +5,7 @@ import Assert.Assert.assertEquals
 /*
 
 https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4512/
+https://leetcode.com/problems/subarray-sum-equals-k/editorial/
 
 Example 4: 560. Subarray Sum Equals K
 
@@ -17,22 +18,33 @@ Run in O(n) and Space O(n)
 */
 
 class SubarraySumEqualsK {
+
     fun subarraySum(nums: IntArray, k: Int): Int {
-        val map = mutableMapOf<Int, Int>()
-        map[0] = 1
-        var curr = 0
+        val sumOccurrencesMap = mutableMapOf<Int, Int>()
+        sumOccurrencesMap[0] = 1
         var ans = 0
-        nums.forEachIndexed { index, value ->
-            curr += value
-            ans += map.getOrDefault(curr - k, 0)
-            map[curr] = map.getOrDefault(curr, 0) + 1
+        var curr = 0
+
+//        If a sub-array sums up to k, then the sum at the end of this sub-array will be sumEnd = sumStart + k.
+//        That implies: sumStart = sumEnd - k.
+
+        nums.forEach {
+            curr += it
+            ans += sumOccurrencesMap.getOrDefault(curr - k, 0)
+            sumOccurrencesMap[curr] = sumOccurrencesMap.getOrDefault(curr, 0) + 1
         }
+
         return ans
     }
+
 }
 
 fun main() {
     val solution1 = SubarraySumEqualsK().subarraySum(intArrayOf(1, 2, 1, 2, 1), 3)
+    val solution2 = SubarraySumEqualsK().subarraySum(intArrayOf(1, 2, 3, 4, 5), 3)
+    val solution3 = SubarraySumEqualsK().subarraySum(intArrayOf(1, 2, 6, 4, 5), 3)
 
     assertEquals(4, solution1)
+    assertEquals(2, solution2)
+    assertEquals(1, solution3)
 }
